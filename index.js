4fa5a282e3efb07a93a7a93a04fa5efccab14018
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-06-24 00:09:21 -0300

    feat: add a post endpoint /participants to signup users

--- index.js
+++ index.js
@@ -4,9 +4,55 @@ import dotenv from "dotenv";
 import {MongoClient, ObjectId} from "mongodb";
 import joi from "joi";
 
+dotenv.config();
+
 const app=express();
 
 app.use([json(),cors()]);
+
+const mongoClient = new MongoClient(process.env.URL_MONGODB);
+
+const participantsSchema=joi.object({
+    name:joi.string().required().min(1),
+})
+
+async function conectionMongoClient (){
+    await mongoClient.connect();
+    return mongoClient.db("api-uol");
+}
+
+function closeMongoClient(){
+    mongoClient.close();
+}
+
+app.post('/participants',async(req,res)=>{
+    const objName=req.body;
+    const validation= participantsSchema.validate(objName);
+    if (validation.error) {
+        const message=validation.error.details[0].message;
+        return res.status(422).send(message);
+    }
+    try{
+        const db= await conectionMongoClient();
+        
+        const userName= await db.collection("participants").findOne({name:objName.name});
+        if(userName){
+            res.status(409).send(`O nome ${objName.name} já está sendo utilizado!`);
+            closeMongoClient();
+            return;
+        }
+        await db.collection("participants").insertOne({
+            name:objName.name,
+            lastStatus: Date.now(),
+        });
+        res.sendStatus(201);
+        closeMongoClient();
+
+    }catch(error){
+        res.status(500).send(error);
+        closeMongoClient();
+    }
+})
 
 
 
